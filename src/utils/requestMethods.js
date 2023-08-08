529aad750480ccdddf2f05fe06756d494cf5387f
vincent <vincent@kali.kali>
2023-08-08 11:38:07 +0300

    added link

--- src/utils/requestMethods.js
+++ src/utils/requestMethods.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 import { useEffect } from "react"
 
-const BASE_URL = "http://localhost:8000/api/"
+const BASE_URL = "https://deskapi.onrender.com/api/"
 
 export const adminRequests = axios.create({
     baseURL:BASE_URL,withCredentials:true
