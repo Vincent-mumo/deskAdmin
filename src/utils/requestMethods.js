f512ff957093a58e2581cbfed735b7069fe45676
vincent <vincent@kali.kali>
2023-08-10 10:27:05 +0300

    added link

--- src/utils/requestMethods.js
+++ src/utils/requestMethods.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 import { useEffect } from "react"
 
-const BASE_URL = "https://desk-api.vercel.app/api/"
+const BASE_URL = "https://deskbackend.onrender.com/api/"
 
 export const adminRequests = axios.create({
     baseURL:BASE_URL,withCredentials:true
